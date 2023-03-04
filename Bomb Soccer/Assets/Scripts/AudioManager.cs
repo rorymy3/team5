162553b67c82223694d08c81a1871a1199e31772
Matthew Johnson <johnsonmh36@gmail.com>
2023-03-04 15:41:31 -0500

    pause menu

--- Bomb Soccer/Assets/Scripts/AudioManager.cs
+++ Bomb Soccer/Assets/Scripts/AudioManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+    public AudioMixerGroup audioMixerGroup;
+    //bool GameisPaused = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -16,6 +18,7 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.outputAudioMixerGroup = audioMixerGroup; //FindObjectOfType<AudioMixerGroup>();
         }
     }
 
@@ -25,6 +28,23 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.PlayOneShot(s.clip);
     }
+
+    public void PauseAll()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source.Pause();
+        }
+    }
+
+    public void PlayAll()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source.Play();
+        }
+    }
+
     public void Stop(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
